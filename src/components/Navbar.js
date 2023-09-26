087b79ab0ceb8847610f6e29b1e8a77dc7622030
Kayode George <gkayode75@gmail.com>
2023-09-26 14:20:02 +0100

    started converting to tailwindcss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,35 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from 'react-router-dom'
 export default function Navbar() {
-	const location = useLocation();
-	return (
-		<nav className="main-navbar">
-			<ul>
-				<NavLink className={location.pathname === "/" ? "nav-link-active" : "nav-link"} to="/" activeStyle>
-					Overview
-				</NavLink>
-				<NavLink className={location.pathname === "/movies" ? "nav-link-active" : "nav-link"} to="/movies" activeStyle>
-					Movies
-				</NavLink>
-				<NavLink className={location.pathname === "/users" ? "nav-link-active" : "nav-link"} to="/users" activeStyle>
-					Users
-				</NavLink>
-			</ul>
-		</nav>
-	);
-};
+  const location = useLocation()
+  return (
+    <nav className='ml-[2%] mr-auto'>
+      <ul>
+        <NavLink
+          className={location.pathname === '/' ? 'nav-link-active' : 'nav-link'}
+          to='/'
+          activeStyle
+        >
+          Overview
+        </NavLink>
+        <NavLink
+          className={
+            location.pathname === '/movies' ? 'nav-link-active' : 'nav-link'
+          }
+          to='/movies'
+          activeStyle
+        >
+          Movies
+        </NavLink>
+        <NavLink
+          className={
+            location.pathname === '/users' ? 'nav-link-active' : 'nav-link'
+          }
+          to='/users'
+          activeStyle
+        >
+          Users
+        </NavLink>
+      </ul>
+    </nav>
+  )
+}
